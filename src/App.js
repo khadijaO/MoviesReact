fc88bfe44cff952166521aee8bd87afc5d017623
khadijaO <61117019+khadijaO@users.noreply.github.com>
2022-04-26 21:00:56 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+const name="khdija"
+// const elem=<h1>hi {name}</h1>
+// const start=()=>React.createElement('h1',null,'hi ')
+  
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Start here.
     </div>
   );
+  // return start();
 }
 
 export default App;
